d50c48c232aa0554574f9ec552c3a5e2fbd07de7
RoxaneBurri <roxane.burri@etu.unige.ch>
2022-06-02 15:45:10 +0200

    Update bar char race and add text

--- barChartRace.js
+++ barChartRace.js
@@ -1,9 +1,16 @@
+// https://observablehq.com/@d3/bar-chart-race@3058
+function _1(md){return(
+    md`# Bar Chart Race
+
+This chart animates the value (in $M) of the top global brands from 2000 to 2019. Color indicates sector. See [the explainer](/d/e9e3929cf7c50b45) for more. Data: [Interbrand](https://www.interbrand.com/best-brands/)`
+)}
+
 function _data(FileAttachment){return(
 FileAttachment("category-brands.csv").csv({typed: true})
 )}
 
 function _replay(html){return(
-html`<button>Replay`
+    html`<button>Replay`
 )}
 
 async function* _chart(replay,d3,width,height,bars,axis,labels,ticker,keyframes,duration,x,invalidation)
@@ -38,36 +45,35 @@ async function* _chart(replay,d3,width,height,bars,axis,labels,ticker,keyframes,
   }
 }
 
-
 function _duration(){return(
-250
+    250
 )}
 
 function _n(){return(
-12
+    12
 )}
 
 function _names(data){return(
-new Set(data.map(d => d.name))
+    new Set(data.map(d => d.name))
 )}
 
 function _datevalues(d3,data){return(
-Array.from(d3.rollup(data, ([d]) => d.value, d => +d.date, d => d.name))
-  .map(([date, data]) => [new Date(date), data])
-  .sort(([a], [b]) => d3.ascending(a, b))
+    Array.from(d3.rollup(data, ([d]) => d.value, d => +d.date, d => d.name))
+        .map(([date, data]) => [new Date(date), data])
+        .sort(([a], [b]) => d3.ascending(a, b))
 )}
 
 function _rank(names,d3,n){return(
-function rank(value) {
-  const data = Array.from(names, name => ({name, value: value(name)}));
-  data.sort((a, b) => d3.descending(a.value, b.value));
-  for (let i = 0; i < data.length; ++i) data[i].rank = Math.min(n, i);
-  return data;
-}
+    function rank(value) {
+      const data = Array.from(names, name => ({name, value: value(name)}));
+      data.sort((a, b) => d3.descending(a.value, b.value));
+      for (let i = 0; i < data.length; ++i) data[i].rank = Math.min(n, i);
+      return data;
+    }
 )}
 
 function _k(){return(
-10
+    10
 )}
 
 function _keyframes(d3,datevalues,k,rank)
@@ -89,170 +95,169 @@ function _keyframes(d3,datevalues,k,rank)
 
 
 function _nameframes(d3,keyframes){return(
-d3.groups(keyframes.flatMap(([, data]) => data), d => d.name)
+    d3.groups(keyframes.flatMap(([, data]) => data), d => d.name)
 )}
 
 function _prev(nameframes,d3){return(
-new Map(nameframes.flatMap(([, data]) => d3.pairs(data, (a, b) => [b, a])))
+    new Map(nameframes.flatMap(([, data]) => d3.pairs(data, (a, b) => [b, a])))
 )}
 
 function _next(nameframes,d3){return(
-new Map(nameframes.flatMap(([, data]) => d3.pairs(data)))
+    new Map(nameframes.flatMap(([, data]) => d3.pairs(data)))
 )}
 
 function _bars(n,color,y,x,prev,next){return(
-function bars(svg) {
-  let bar = svg.append("g")
-      .attr("fill-opacity", 0.6)
-    .selectAll("rect");
+    function bars(svg) {
+      let bar = svg.append("g")
+          .attr("fill-opacity", 0.6)
+          .selectAll("rect");
 
-  return ([date, data], transition) => bar = bar
-    .data(data.slice(0, n), d => d.name)
-    .join(
-      enter => enter.append("rect")
-        .attr("fill", color)
-        .attr("height", y.bandwidth())
-        .attr("x", x(0))
-        .attr("y", d => y((prev.get(d) || d).rank))
-        .attr("width", d => x((prev.get(d) || d).value) - x(0)),
-      update => update,
-      exit => exit.transition(transition).remove()
-        .attr("y", d => y((next.get(d) || d).rank))
-        .attr("width", d => x((next.get(d) || d).value) - x(0))
-    )
-    .call(bar => bar.transition(transition)
-      .attr("y", d => y(d.rank))
-      .attr("width", d => x(d.value) - x(0)));
-}
+      return ([date, data], transition) => bar = bar
+          .data(data.slice(0, n), d => d.name)
+          .join(
+              enter => enter.append("rect")
+                  .attr("fill", color)
+                  .attr("height", y.bandwidth())
+                  .attr("x", x(0))
+                  .attr("y", d => y((prev.get(d) || d).rank))
+                  .attr("width", d => x((prev.get(d) || d).value) - x(0)),
+              update => update,
+              exit => exit.transition(transition).remove()
+                  .attr("y", d => y((next.get(d) || d).rank))
+                  .attr("width", d => x((next.get(d) || d).value) - x(0))
+          )
+          .call(bar => bar.transition(transition)
+              .attr("y", d => y(d.rank))
+              .attr("width", d => x(d.value) - x(0)));
+    }
 )}
 
 function _labels(n,x,prev,y,next,textTween){return(
-function labels(svg) {
-  let label = svg.append("g")
-      .style("font", "bold 12px var(--sans-serif)")
-      .style("font-variant-numeric", "tabular-nums")
-      .attr("text-anchor", "end")
-    .selectAll("text");
+    function labels(svg) {
+      let label = svg.append("g")
+          .style("font", "bold 12px var(--sans-serif)")
+          .style("font-variant-numeric", "tabular-nums")
+          .attr("text-anchor", "end")
+          .selectAll("text");
 
-  return ([date, data], transition) => label = label
-    .data(data.slice(0, n), d => d.name)
-    .join(
-      enter => enter.append("text")
-        .attr("transform", d => `translate(${x((prev.get(d) || d).value)},${y((prev.get(d) || d).rank)})`)
-        .attr("y", y.bandwidth() / 2)
-        .attr("x", -6)
-        .attr("dy", "-0.25em")
-        .text(d => d.name)
-        .call(text => text.append("tspan")
-          .attr("fill-opacity", 0.7)
-          .attr("font-weight", "normal")
-          .attr("x", -6)
-          .attr("dy", "1.15em")),
-      update => update,
-      exit => exit.transition(transition).remove()
-        .attr("transform", d => `translate(${x((next.get(d) || d).value)},${y((next.get(d) || d).rank)})`)
-        .call(g => g.select("tspan").tween("text", d => textTween(d.value, (next.get(d) || d).value)))
-    )
-    .call(bar => bar.transition(transition)
-      .attr("transform", d => `translate(${x(d.value)},${y(d.rank)})`)
-      .call(g => g.select("tspan").tween("text", d => textTween((prev.get(d) || d).value, d.value))));
-}
+      return ([date, data], transition) => label = label
+          .data(data.slice(0, n), d => d.name)
+          .join(
+              enter => enter.append("text")
+                  .attr("transform", d => `translate(${x((prev.get(d) || d).value)},${y((prev.get(d) || d).rank)})`)
+                  .attr("y", y.bandwidth() / 2)
+                  .attr("x", -6)
+                  .attr("dy", "-0.25em")
+                  .text(d => d.name)
+                  .call(text => text.append("tspan")
+                      .attr("fill-opacity", 0.7)
+                      .attr("font-weight", "normal")
+                      .attr("x", -6)
+                      .attr("dy", "1.15em")),
+              update => update,
+              exit => exit.transition(transition).remove()
+                  .attr("transform", d => `translate(${x((next.get(d) || d).value)},${y((next.get(d) || d).rank)})`)
+                  .call(g => g.select("tspan").tween("text", d => textTween(d.value, (next.get(d) || d).value)))
+          )
+          .call(bar => bar.transition(transition)
+              .attr("transform", d => `translate(${x(d.value)},${y(d.rank)})`)
+              .call(g => g.select("tspan").tween("text", d => textTween((prev.get(d) || d).value, d.value))));
+    }
 )}
 
 function _textTween(d3,formatNumber){return(
-function textTween(a, b) {
-  const i = d3.interpolateNumber(a, b);
-  return function(t) {
-    this.textContent = formatNumber(i(t));
-  };
-}
+    function textTween(a, b) {
+      const i = d3.interpolateNumber(a, b);
+      return function(t) {
+        this.textContent = formatNumber(i(t));
+      };
+    }
 )}
 
 function _formatNumber(d3){return(
-d3.format(",d")
+    d3.format(",d")
 )}
 
 function _tickFormat(){return(
-undefined
+    undefined
 )}
 
 function _axis(margin,d3,x,width,tickFormat,barSize,n,y){return(
-function axis(svg) {
-  const g = svg.append("g")
-      .attr("transform", `translate(0,${margin.top})`);
+    function axis(svg) {
+      const g = svg.append("g")
+          .attr("transform", `translate(0,${margin.top})`);
 
-  const axis = d3.axisTop(x)
-      .ticks(width / 160, tickFormat)
-      .tickSizeOuter(0)
-      .tickSizeInner(-barSize * (n + y.padding()));
+      const axis = d3.axisTop(x)
+          .ticks(width / 160, tickFormat)
+          .tickSizeOuter(0)
+          .tickSizeInner(-barSize * (n + y.padding()));
 
-  return (_, transition) => {
-    g.transition(transition).call(axis);
-    g.select(".tick:first-of-type text").remove();
-    g.selectAll(".tick:not(:first-of-type) line").attr("stroke", "white");
-    g.select(".domain").remove();
-  };
-}
+      return (_, transition) => {
+        g.transition(transition).call(axis);
+        g.select(".tick:first-of-type text").remove();
+        g.selectAll(".tick:not(:first-of-type) line").attr("stroke", "white");
+        g.select(".domain").remove();
+      };
+    }
 )}
 
 function _ticker(barSize,width,margin,n,formatDate,keyframes){return(
-function ticker(svg) {
-  const now = svg.append("text")
-      .style("font", `bold ${barSize}px var(--sans-serif)`)
-      .style("font-variant-numeric", "tabular-nums")
-      .attr("text-anchor", "end")
-      .attr("x", width - 6)
-      .attr("y", margin.top + barSize * (n - 0.45))
-      .attr("dy", "0.32em")
-      .text(formatDate(keyframes[0][0]));
+    function ticker(svg) {
+      const now = svg.append("text")
+          .style("font", `bold ${barSize}px var(--sans-serif)`)
+          .style("font-variant-numeric", "tabular-nums")
+          .attr("text-anchor", "end")
+          .attr("x", width - 6)
+          .attr("y", margin.top + barSize * (n - 0.45))
+          .attr("dy", "0.32em")
+          .text(formatDate(keyframes[0][0]));
 
-  return ([date], transition) => {
-    transition.end().then(() => now.text(formatDate(date)));
-  };
-}
+      return ([date], transition) => {
+        transition.end().then(() => now.text(formatDate(date)));
+      };
+    }
 )}
 
 function _formatDate(d3){return(
-d3.utcFormat("%Y")
+    d3.utcFormat("%Y")
 )}
 
 function _color(d3,data)
 {
   const scale = d3.scaleOrdinal(d3.schemeTableau10);
-/*  if (data.some(d => d.category !== undefined)) {
-    const categoryByName = new Map(data.map(d => d.name))
+  if (data.some(d => d.category !== undefined)) {
+    const categoryByName = new Map(data.map(d => [d.name, d.category]))
     scale.domain(categoryByName.values());
     return d => scale(categoryByName.get(d.name));
-  }*/
+  }
   return d => scale(d.name);
 }
 
-
 function _x(d3,margin,width){return(
-d3.scaleLinear([0, 1], [margin.left, width - margin.right])
+    d3.scaleLinear([0, 1], [margin.left, width - margin.right])
 )}
 
 function _y(d3,n,margin,barSize){return(
-d3.scaleBand()
-    .domain(d3.range(n + 1))
-    .rangeRound([margin.top, margin.top + barSize * (n + 1 + 0.1)])
-    .padding(0.1)
+    d3.scaleBand()
+        .domain(d3.range(n + 1))
+        .rangeRound([margin.top, margin.top + barSize * (n + 1 + 0.1)])
+        .padding(0.1)
 )}
 
 function _height(margin,barSize,n){return(
-margin.top + barSize * n + margin.bottom
+    margin.top + barSize * n + margin.bottom
 )}
 
 function _barSize(){return(
-48
+    48
 )}
 
 function _margin(){return(
-{top: 16, right: 6, bottom: 6, left: 0}
+    {top: 16, right: 6, bottom: 6, left: 0}
 )}
 
 function _d3(require){return(
-require("d3@6")
+    require("d3@6")
 )}
 
 export default function define(runtime, observer) {
@@ -262,6 +267,7 @@ export default function define(runtime, observer) {
     ["category-brands.csv", {url: new URL("./data/starshipsRace.csv", import.meta.url), mimeType: "text/csv", toString}]
   ]);
   main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
+  main.variable(observer()).define(["md"], _1);
   main.variable(observer("data")).define("data", ["FileAttachment"], _data);
   main.variable(observer("viewof replay")).define("viewof replay", ["html"], _replay);
   main.variable(observer("replay")).define("replay", ["Generators", "viewof replay"], (G, _) => G.input(_));
